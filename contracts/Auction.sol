91df46b9be7730a97f006a12ae687f38dc6bf51d
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2023-05-18 18:37:30 +0100

    Auction Contract Completed - #0005

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -12,19 +12,19 @@ contract Auction {
         string name;
         string description;
         uint marketValue;
+        uint bidValue;
     }
     Item public AuctionItem;
     uint public totalBid;
     uint internal recentBid;
+    uint public finalBid;
     address internal recentBidder;
-    address[] public bidders;
-    mapping (address => uint) public bidderFunds;
 
 
     constructor(string memory _name, string memory _description, uint _amountInETH) {
         _status = NotEntered;
         owner = msg.sender;
-        AuctionItem = Item(block.timestamp, _name, _description, _amountInETH * (10**18));
+        AuctionItem = Item(block.timestamp, _name, _description, _amountInETH * (10**18), 0);
         recentBid = 0 wei;
      }
 
@@ -42,16 +42,17 @@ contract Auction {
     }
 
     function _addBid() private {
-        bidderFunds[msg.sender] += msg.value;
-        recentBid = bidderFunds[msg.sender];
-        recentBidder = msg.sender;
-        totalBid += msg.value;
-
-        for (uint i = 0; i < bidders.length; i++) {
-            if(msg.sender != bidders[i]) {
-                bidders.push(msg.sender);
-            }
+        if (recentBidder == address(0)) {
+            recentBidder = msg.sender;
+            recentBid = msg.value;
+        } else 
+        {
+            payable(recentBidder).transfer(recentBid);
+            recentBidder = msg.sender;
+            recentBid = msg.value;
         }
+        AuctionItem.bidValue = recentBid;
+        totalBid += msg.value;
     }
 
     function placeBid() payable public virtual nonReentrancy {
@@ -73,15 +74,8 @@ contract Auction {
     }
     
     function rewardHighestBid() public payable nonReentrancy onlyOwner {
-        for (uint i = 0; i < bidders.length; i++) {
-            if (bidders[i] != recentBidder) {
-                uint refund = bidderFunds[bidders[i]];
-                payable(bidders[i]).transfer(refund);
-            } else 
-            {
-                payable(owner).transfer(highestBid());
-            }    
-        }
+        payable(owner).transfer(highestBid());
+        finalBid = recentBid;
         recentBid = 0;
     }
 
